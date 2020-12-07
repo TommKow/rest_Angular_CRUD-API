288ae3b005fcb1537365df49c24c7c1c0880677d
Tomasz Kowalski <tomasz.kowalski87@yahoo.com>
2020-12-07 22:32:19 +0100

    REST controller getAllEmployees, exception, model emploee, repository employee

---
+++ src/main/java/com/backend/rest/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.backend.rest.controller;
+
+import com.backend.rest.model.Employee;
+import com.backend.rest.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+}
