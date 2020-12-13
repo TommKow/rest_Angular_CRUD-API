0a44d9fcabf9d384a621e0999e059e6d86763698
Tomasz Kowalski <tomasz.kowalski87@yahoo.com>
2020-12-13 22:31:30 +0100

    Backend - Add CrosOrigin to employeeController

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private baseURL = "http://localhost:8080/api/v1/employees";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
 }
--- src/main/java/com/backend/rest/controller/EmployeeController.java
+++ src/main/java/com/backend/rest/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import com.backend.rest.model.Employee;
 import com.backend.rest.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
